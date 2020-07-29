537cdb76dc03c6e73042120f09226303352c46a1
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2020-07-29 11:36:49 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Öğrenci Bilgilendirme Sistemi</title>
+</head>
+<body>
+    <form action="">
+        <label for="studentNumber">Öğrenci Numarası:</label><br>
+        <input type="" id="studentNumber" name="studentNumber" value="160707000"><br>
+    </form>
+</body>
+</html>

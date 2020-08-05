32e561e0cebbd741fc56f9258cbb4bab5fcabc82
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2020-08-05 23:11:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/style.css" rel="stylesheet" />
+    
     <title>Öğrenci Bilgilendirme Sistemi</title>
 </head>
+
 <body>
     <form action="">
         <label for="studentNumber">Öğrenci Numarası:</label><br>
-        <input type="" id="studentNumber" name="studentNumber" value="160707000"><br>
+        <input type="text" id="studentNumber" name="studentNumber" placeholder="Öğrenci Nu"><br>
+        <label for="password">Parola:</label><br>
+        <input type="password" id="password" name="password" placeholder="Parolanız"><br><br>
+        <input type="submit" value="Giriş Yap">
     </form>
 </body>
 </html>

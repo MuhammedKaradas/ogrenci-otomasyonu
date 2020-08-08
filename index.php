b7b1442a233164a1c83e1c0256ecbcaf4c3e3408
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2020-08-08 19:27:49 +0300

    css düzenlemesi

--- index.php
+++ index.php
@@ -4,11 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/css/style.css" rel="stylesheet" />
-    
+    <!-- BOOTSTRAP STYLES-->
+  <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <title>Öğrenci Bilgilendirme Sistemi</title>
+    <style>
+    .kutu {
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 25px;
+    }
+    .satir {
+
+    }
+    </style>
 </head>
 
 <body>
+<div class="kutu">
     <form action="">
         <label for="studentNumber">Öğrenci Numarası:</label><br>
         <input type="text" id="studentNumber" name="studentNumber" placeholder="Öğrenci Nu"><br>
@@ -16,5 +28,9 @@
         <input type="password" id="password" name="password" placeholder="Parolanız"><br><br>
         <input type="submit" value="Giriş Yap">
     </form>
+</div>
+
+
+
 </body>
 </html>
